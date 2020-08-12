48ef66564c91e32268d1fbc0eb2457509a883da5
Stephan Ellenberger <s.ellenberger95@gmail.com>
2020-08-12 11:26:09 +0200

    Added google analytics to the app component

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Navbar, Landing } from '../index'
 import classes from './App.module.scss'
+import ReactGa from 'react-ga'
 
 function App() {
+  useEffect(() => {
+    ReactGa.initialize('XX-XXXXXXXX-XX')
+
+    // to report page view
+    ReactGa.pageview(window.location.pathname + window.location.search)
+  }, [])
   return (
       <div className={classes.appContentContainer}>
         <Navbar />
