ca731be65354ae63acdca90c388fa8ed0fbec63c
Stephan Ellenberger <s.ellenberger95@gmail.com>
2020-08-16 15:21:32 +0200

    About section added

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import classes from './About.module.scss'
-
+import aboutPhoto from '../../media/aboutPhoto.jpg'
 const About = () => {
   return (
-    <div>I am the About component</div>
+    <div className={classes.aboutContainer}>
+      <div className={classes.sideNav}>
+        <a href=""><div>Biography</div></a> <br/>
+        <a href=""><div>Awards</div></a>
+      </div>
+      <div className={classes.aboutContent}>
+        <img src={aboutPhoto} alt="gabriella jones standing at the harp" className={classes.aboutPhoto}/>
+        <p className={classes.aboutBio}>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum. <br/> Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum</p>
+      </div>
+    </div>
   )
 }
 
