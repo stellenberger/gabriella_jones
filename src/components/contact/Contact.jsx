7c1dc4c2b615fed044c54dd1cf81d4518eebbfbc
Stephan Ellenberger <s.ellenberger95@gmail.com>
2020-08-17 15:42:34 +0200

    Contact section added

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
 import classes from './Contact.module.scss'
-
+import contactImage from '../../media/contactImage.jpg'
 const Contact = () => {
+  const managementDetails = [{name: 'John Smith', mobile_number: '+44 7893 944 031', email: 'john@smith.com'}]
+  const personalDetails = [{name: 'Gabriella Jones', mobile_number: '+44 7893 944 031', email: 'gsjones@btinternet.com'}]
   return (
-    <div>I am the contact component</div>
+    <div className={classes.contactContainer}>
+      <div className={classes.contactImage}>
+        <img src={contactImage} alt="Gabriella playing the harp" />
+      </div>
+
+      <div className={classes.contactInformation}>
+        { managementDetails && managementDetails.map((management) => {
+          return (
+            <div className={classes.managementContactDetails}>
+              <p className={classes.contactTitle}>General Management</p>
+              <p>{management.name}</p>
+              <p>{management.email}</p>
+              <p>{management.mobile_number}</p>
+            </div>
+          )
+        })}
+        { personalDetails && personalDetails.map((personal) => {
+          return (
+            <div className={classes.personalContactDetails}>
+              <p className={classes.contactTitle}>Personal Contact</p>
+              <p>{personal.name}</p>
+              <p>{personal.email}</p>
+              <p>{personal.mobile_number}</p>
+            </div>
+          )
+        })}
+      </div>
+    </div>
   )
 }
 
