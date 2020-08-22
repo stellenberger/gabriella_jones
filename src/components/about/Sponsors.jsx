047003665fba1936bca5ce236e754f5719ce1900
Stephan Ellenberger <s.ellenberger95@gmail.com>
2020-08-22 17:48:50 +0100

    Changes made to Mobile view of About and Schedule pages

--- src/components/about/Sponsors.jsx
+++ src/components/about/Sponsors.jsx
@@ -6,10 +6,10 @@ import { leverhulme, helpMusicians, makingMusic, parkLane } from '../../media'
 const Sponsors = () => {
   return (
     <div className={classes.sponsors}>
-      <a href="https://www.leverhulme.ac.uk/"><img src={leverhulme} alt=""/></a>
-      <a href="https://www.helpmusicians.org.uk/"><img src={helpMusicians} alt=""/></a>
-      <a href="https://www.makingmusic.org.uk/"><img src={makingMusic} alt=""/></a>
-      <a href="https://www.parklanegroup.co.uk/"><img src={parkLane} alt=""/></a>
+      <a href="https://www.leverhulme.ac.uk/"><img src={leverhulme} alt="Leverhulme Trust"/></a>
+      <a href="https://www.helpmusicians.org.uk/"><img src={helpMusicians} alt="Help Musicians UK"/></a>
+      <a href="https://www.makingmusic.org.uk/"><img src={makingMusic} alt="Making Music UK"/></a>
+      <a href="https://www.parklanegroup.co.uk/"><img src={parkLane} alt="Park Lane Group"/></a>
     </div>
   )
 }
