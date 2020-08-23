12a75296fa6003d66f6a790a2fd0baee0023fabf
Stephan Ellenberger <s.ellenberger95@gmail.com>
2020-08-23 17:16:24 +0100

    Added Repertoire and ADeux duo on the about apge

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react'
 import classes from './About.module.scss'
 import Biography from './Biography'
 import Awards from './Awards'
+import ADeux from './ADeux'
+import Repertoire from './Repertoire'
 import Sponsors from './Sponsors'
 
 
@@ -11,14 +13,31 @@ const About = () => {
   const handleClick = (page) => {
     setSubPage(page)
   }
+
+  const loadPage = () => {
+    switch (subPage) {
+      case 'biography':
+        return <Biography />;
+      case 'awards':
+        return <Awards />;
+      case 'adeux':
+        return <ADeux />;
+      case 'repertoire':
+        return <Repertoire />;
+      default:
+        return null;
+    }
+  }
   return (
     <div className={classes.aboutContainer}>
       <div className={classes.sideNav}>
         <a href="/about#biography" onClick={() => handleClick('biography')}><div className={classes.links}>Biography</div></a>
         <a href="/about#awards" onClick={() => handleClick('awards')}><div className={classes.links}>Awards</div></a>
+        <a href="/about#repertoire" onClick={() => handleClick('repertoire')}><div className={classes.links}>Repertoire</div></a>
+        <a href="/about#adeux" onClick={() => handleClick('adeux')}><div className={classes.links}>A Deux Duo</div></a>
       </div>
       <div className={classes.aboutContent}>
-        {subPage === 'biography' ? (<Biography />) : (<Awards />)}
+        { loadPage() }
         <Sponsors />
       </div>
     </div>
---
+++ src/components/about/Repertoire.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import repertoire from '../../information/repertoire.json'
+import classes from './About.module.scss'
+
+const Repertoire = () => {
+  return (
+    <div>
+      {repertoire && repertoire.repertoire.map(repertoire => {
+        return (
+          <div>
+            <p className={classes.repertoireTitle}>Repertoire</p>
+            <p className={classes.repertoireTypeTitle}>Solo</p>
+            {repertoire.solo.map(piece => {
+              return (
+              <p className={classes.repertoireName}>{piece.composer} - {piece.piece}</p>
+              )
+            })}
+            <p className={classes.repertoireTypeTitle}>Solo Transcriptions</p>
+            {repertoire.soloTranscriptions.map(piece => {
+              return (
+              <p className={classes.repertoireName}>{piece.composer} - {piece.piece}</p>
+              )
+            })}
+            <p className={classes.repertoireTypeTitle}>Concerti</p>
+            {repertoire.concerti.map(piece => {
+              return (
+              <p className={classes.repertoireName}>{piece.composer} - {piece.piece}</p>
+              )
+            })}
+            <p className={classes.repertoireTypeTitle}>Contemporary Music</p>
+            {repertoire.contemporaryMusic.map(piece => {
+              return (
+              <p className={classes.repertoireName}>{piece.composer} - {piece.piece}</p>
+              )
+            })}
+            <p className={classes.repertoireTypeTitle}>Harp and Choir</p>
+            {repertoire.harpChoir.map(piece => {
+              return (
+              <p className={classes.repertoireName}>{piece.composer} - {piece.piece}</p>
+              )
+            })}
+            <p className={classes.repertoireTypeTitle}>Chamber Music</p>
+            {repertoire.chamberMusic.map(piece => {
+              return (
+              <p className={classes.repertoireName}>{piece.composer} - {piece.piece}</p>
+              )
+            })}
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+
+export default Repertoire
