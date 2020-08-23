a4a55f8747a4a5f1119e5807d20c6b8c4897ed31
Stephan Ellenberger <s.ellenberger95@gmail.com>
2020-08-23 14:45:07 +0100

    Images replaced with smaller ones for optimum SEO and load speed

--- src/media/index.jsx
+++ src/media/index.jsx
@@ -1,3 +1,4 @@
+export { default as Hero } from './hero.png'
 export { default as Instagram } from './instagram.svg'
 export { default as Twitter } from './twitter.svg'
 export { default as YouTube } from './youtube.svg'
