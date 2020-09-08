ce5a2c365d1b2e582ad1ef13ef96c1b379a9100a
Stephan Ellenberger <s.ellenberger95@gmail.com>
2020-09-08 11:20:38 +0100

    Changed some information and video links

--- src/components/media/Videos.jsx
+++ src/components/media/Videos.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import classes from './Media.module.scss'
 import ReactPlayer from 'react-player';
+import video_links from '../../information/video_links.json'
 
 const Videos = () => {
-  const video_link = 'https://www.youtube.com/watch?v=J0MHSoiHxEE&t=361s'
   return (
     <div>
       <p className={classes.mediaTitle}>Videos</p>
       <div className={classes.videoContainer}>
         <div className={classes.videoWrapper}>  
-          <ReactPlayer className={classes.video} url={video_link} width="100%" height="45vh" />
+          {video_links.map(video => {
+            return <ReactPlayer className={classes.video} url={video.video_link} width="100%" height="45vh" />
+          })}
         </div>
       </div>
     </div>
