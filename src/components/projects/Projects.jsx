239429bc3e342ac735d395e7959520059b6830a3
Stephan Ellenberger <stephan.ellenberger@justeattakeaway.com>
2025-02-07 17:05:02 +0800

    Put the projects in the project section

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,8 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./Project.module.scss";
 
 const Project = () => {
-  return <div className={classes.projectContainer}>Projects</div>;
+  const [project, setProject] = useState("");
+
+  const projects = [
+    { id: 1, project: "[UN]Love" },
+    { id: 1, project: "Inner Noise" },
+    { id: 2, project: "Freude" },
+  ];
+
+  const handleProjectClick = (project) => {
+    setProject(project);
+  };
+
+  return (
+    <div className={classes.projectContainer}>
+      <div className={classes.projectNavigator}>
+        {projects &&
+          projects.map((project) => {
+            return (
+              <p
+                className={classes.project}
+                id={project.id}
+                onClick={() => handleProjectClick(project.project)}
+              >
+                {project.project}
+              </p>
+            );
+          })}
+      </div>
+      <p>Projects</p>
+    </div>
+  );
 };
 
 export default Project;
