eb06ba6537085da85e606ffe8c6861c15c35d857
Stephan Ellenberger <stephan.ellenberger@justeattakeaway.com>
2025-02-07 17:32:35 +0800

    Add basic layout for unlove project

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,21 +1,32 @@
 import React, { useState } from "react";
 import classes from "./Project.module.scss";
+import Unlove from "./Unlove.tsx";
 
 const Project = () => {
   const [project, setProject] = useState("");
 
   const projects = [
-    { id: 1, project: "[UN]Love" },
-    { id: 1, project: "Inner Noise" },
-    { id: 2, project: "Freude" },
+    { id: 1, project: "[UN]Love", accessor: "unlove" },
+    { id: 1, project: "Inner Noise", accessor: "inner_noise" },
+    { id: 2, project: "Freude", accessor: "freude" },
   ];
 
   const handleProjectClick = (project) => {
     setProject(project);
   };
 
+  const renderProject = () => {
+    switch (project) {
+      case "unlove":
+        return <Unlove />;
+      default:
+        return null;
+    }
+  };
+
   return (
     <div className={classes.projectContainer}>
+      <div className={classes.projectNavigator}>{renderProject()}</div>
       <div className={classes.projectNavigator}>
         {projects &&
           projects.map((project) => {
