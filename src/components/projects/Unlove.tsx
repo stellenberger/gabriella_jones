72c29ed696e066c611797a8610dca4b8d930dad6
Stephan Ellenberger <stephan.ellenberger@justeattakeaway.com>
2025-04-08 21:10:09 +0200

    Update unloved

--- src/components/projects/Unlove.tsx
+++ src/components/projects/Unlove.tsx
@@ -3,15 +3,15 @@ import React from "react";
 export default function Unlove() {
   return (
     <div>
-      <div className={"flex flex-between items-center bg-black"}>
-        <p>
+      <div className={"flex justify-between items-center mb-5 w-xl"}>
+        <p className={"text-2xl font-bold"}>
           <b>Introduction</b>
         </p>
-        <p>
-          <b>Introduction</b>
+        <p className={"text-5xl font-bold"}>
+          <b>[UN]Love</b>
         </p>
       </div>
-      <p>
+      <p className={"mb-5"}>
         The title [UN]Love encapsulates the duality of love's presence and
         absence. The prefix “UN” evokes themes of loss, unrequited love, and the
         experience of being unloved, while the brackets suggest that love can
@@ -19,7 +19,7 @@ export default function Unlove() {
         entirely. This dichotomy reflects love's complexity and its ability to
         persist, transform, or unravel in different contexts.
       </p>
-      <p>
+      <p className={"mb-5"}>
         [UN]Love is a solo harp project that delves into the complexities of
         love through the voices of historically underrepresented female
         composers. Love has always been a central theme in art, but its
@@ -27,12 +27,12 @@ export default function Unlove() {
         male perspectives, focusing on themes such as heartbreak, scorn, or
         tragedy.
       </p>
-      <p>
+      <p className={"mb-5"}>
         This project aims to amplify alternative perspectives: maternal love,
         the origins and fragility of love, female grief, and the ways women are
         loved and viewed.
       </p>
-      <p>
+      <p className={"mb-5"}>
         The harp, historically associated with elegance, femininity, and
         refinement, has long been tied to specific cultural images—graceful
         women performing in French salons, or angelic figures in romanticized
@@ -41,7 +41,7 @@ export default function Unlove() {
         viewed as an instrument of beauty and delicacy, the harp's potential for
         power, versatility, and modern expression has often been overlooked.
       </p>
-      <p>
+      <p className={"mb-5"}>
         In 2025, this traditional association is evolving. The harp is
         increasingly being used as a vehicle for bold, innovative works that
         challenge its conventional image, with composers and performers pushing
@@ -51,19 +51,37 @@ export default function Unlove() {
         highlights the harp's role in reimagining its identity—not just as an
         emblem of grace but as a powerful medium for storytelling.
       </p>
-      <p>
+      <p className={"mb-5"}>
         Through this project, I will independently release recordings for four
         works, three of which will be premiere recordings, distributed on major
         streaming platforms including Spotify and Apple Music.
       </p>
-      <p>
+      <div className="mb-5">
+        <p className="font-semibold mb-2">The repertoire includes:</p>
+        <ul className="list-disc list-inside space-y-1">
+          <li>
+            A commissioned work from the Park Lane Group, supported by the
+            Vaughan Williams Foundation in 2019.
+          </li>
+          <li>
+            An arrangement of a piano work by a prominent British female
+            composer.
+          </li>
+          <li>
+            An unpublished and unrecorded work from 1912 by one of the most
+            eminent harpists and composers of the 20th century.
+          </li>
+        </ul>
+      </div>
+
+      <p className={"mb-5"}>
         Each piece in [UN]LOVE will be accompanied by visually poignant videos
         filmed in locations that resonate with the style, emotion, and story of
         the music. This approach not only highlights the music but also broadens
         its reach across multiple platforms, creating a more immersive and
         accessible experience.
       </p>
-      <p>
+      <p className={"mb-5"}>
         Inspired by James Rhodes' Live in Brighton, I will include bonus
         speaking tracks with each release in the [UN]LOVE project. These tracks
         aim to create a personal and intimate listening experience by sharing
@@ -73,7 +91,7 @@ export default function Unlove() {
         connection to the music by revealing the human stories and emotions
         intertwined with it.
       </p>
-      <p>
+      <p className={"mb-5"}>
         Beyond personal storytelling, these speaking tracks serve to make the
         music more accessible to a wider audience. By demystifying the pieces
         and explaining their significance in an approachable way, I hope to
@@ -83,14 +101,14 @@ export default function Unlove() {
         valuable insight into the evolution of the harp's repertoire and the
         narratives that shaped [UN]LOVE.
       </p>
-      <p>
+      <p className={"mb-5"}>
         Incorporating spoken tracks transforms this project into more than just
         an album—it becomes a dialogue, a shared exploration of love, music, and
         the stories we tell through art. I hope it creates a lasting connection
         not just to the music, but to the creative journey behind it.
       </p>
       <b>Why?</b>
-      <p>
+      <p className={"mb-5"}>
         After a decade of performing new music without recording it, I've come
         to appreciate the importance of preserving these works—not just for
         myself, but for anyone who might find meaning in them. Each piece in
@@ -98,13 +116,13 @@ export default function Unlove() {
         moments it represents in my life or the unique circumstances that
         brought it into existence.
       </p>
-      <p>
+      <p className={"mb-5"}>
         This project is not simply about creating an audio legacy—it's about
         sharing the stories and emotions that have shaped me as an artist. It's
         my way of ensuring that these works, and the feelings they evoke,
         continue to inspire and connect with others long into the future.
       </p>
-      <p>
+      <p className={"mb-5"}>
         This deliberate effort seeks to preserve and revitalize compositions
         that might otherwise be lost to the repertoire, diversifying and
         enriching the future of harp music.
